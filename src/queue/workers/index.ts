21770f0c8c3598c36b18f3909d525169a86b398f
Smejia11 <146378532+Smejia11@users.noreply.github.com>
2024-09-12 21:20:14 -0500

    fix: fix minor change
    fix: fix minor change

--- src/queue/workers/index.ts
+++ src/queue/workers/index.ts
@@ -1,5 +1,5 @@
 import { Worker } from 'bullmq';
 import { connection, concurrency } from '../config';
-import workerTest from './tes.worker';
+import workerTest from './test.worker';
 
 new Worker('testQueue', workerTest, { connection, concurrency });
--- src/queue/workers/test.worker.ts
+++ src/queue/workers/test.worker.ts
@@ -3,10 +3,12 @@ import { sleep } from '../../utils';
 
 const workerTest = async (job: Job) => {
   try {
+    await job.updateProgress(1);
     await sleep(20000);
+    await job.updateProgress(100);
     return { error: false, result: 'completado' };
-  } catch (error: any) {
-    throw new Error(error);
+  } catch (error: unknown) {
+    throw new Error('failed process worker ', { cause: error });
   }
 };
 
